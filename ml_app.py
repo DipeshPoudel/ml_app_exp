cbcb6c3a9360bb0d13d9558f9cec3a5d9cb32e54
DipeshPoudel <dpshpoudel101@gmail.com>
2025-12-18 07:50:10 +0545

    Second Chnages

--- ml_app.py
+++ ml_app.py
@@ -4,3 +4,4 @@ st.title("My New Application")
 
 age = st.sidebar.slider('Age',min_value=1,max_value=100,step=1)
 gender = st.sidebar.selectbox('Gender',['Male','Female','Others'])
+smoking = st.sidebar.selectbox('Smoking',['Yes','No'])
